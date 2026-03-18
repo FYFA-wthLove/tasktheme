e298ad176c698ded7a6df2aac4929cb7797fe22a
Dyadechko Maxim <maxim.fyfa1997@gmail.com>
2026-03-18 08:27:11 +0200

    Init hero block

--- functions.php
+++ functions.php
@@ -8,22 +8,37 @@
  * @return void
  */
 function mytheme_enqueue_assets() {
-    wp_enqueue_style(
-        'mytheme-style',
-        get_template_directory_uri() . '/assets/dist/main.css',
-        [],
-        '1.0'
-    );
+	wp_enqueue_style(
+		'mytheme-style',
+		get_template_directory_uri() . '/assets/dist/main.css',
+		[],
+		'1.0'
+	);
 
-    wp_enqueue_script(
-        'mytheme-script',
-        get_template_directory_uri() . '/assets/dist/main.js',
-        [],
-        '1.0',
-        true
-    );
+	wp_enqueue_script(
+		'mytheme-script',
+		get_template_directory_uri() . '/assets/dist/main.js',
+		[],
+		'1.0',
+		true
+	);
+
+	wp_enqueue_style(
+		'swiper-css',
+		'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.css',
+		[],
+		null
+	);
+
+	wp_enqueue_script(
+		'swiper-js',
+		'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.js',
+		array(),
+		null,
+		true
+	);
 }
-add_action('wp_enqueue_scripts', 'mytheme_enqueue_assets');
+add_action( 'wp_enqueue_scripts', 'mytheme_enqueue_assets' );
 
 /**
  * Setup theme defaults and register supported features.
@@ -36,15 +51,15 @@ add_action('wp_enqueue_scripts', 'mytheme_enqueue_assets');
  * @return void
  */
 function mytheme_setup() {
-    add_theme_support('title-tag');
-    add_theme_support('post-thumbnails');
-    add_theme_support('editor-styles');
+	add_theme_support( 'title-tag' );
+	add_theme_support( 'post-thumbnails' );
+	add_theme_support( 'editor-styles' );
 
-    register_nav_menus([
-        'primary' => 'Primary Menu'
-    ]);
+	register_nav_menus( [
+		'primary' => 'Primary Menu',
+	] );
 }
-add_action('after_setup_theme', 'mytheme_setup');
+add_action( 'after_setup_theme', 'mytheme_setup' );
 
 /**
  * Register custom page templates located in /templates directory.
@@ -52,13 +67,13 @@ add_action('after_setup_theme', 'mytheme_setup');
  * @param array $templates Existing page templates.
  * @return array Modified list of templates.
  */
-function mytheme_page_templates($templates) {
-    $templates['templates/template-home.php'] = 'Home Page1';
-    $templates['templates/template-fullwidth.php'] = 'Full Width';
+function mytheme_page_templates( $templates ) {
+	$templates['templates/template-home.php']      = 'Home Page';
+	$templates['templates/template-fullwidth.php'] = 'Full Width';
 
-    return $templates;
+	return $templates;
 }
-add_filter('theme_page_templates', 'mytheme_page_templates');
+add_filter( 'theme_page_templates', 'mytheme_page_templates' );
 
 /**
  * Load custom page templates from /templates directory.
@@ -68,14 +83,31 @@ add_filter('theme_page_templates', 'mytheme_page_templates');
  * @param string $template Default template path.
  * @return string Modified template path.
  */
-function mytheme_load_template($template) {
-    if (is_page()) {
-        $meta = get_post_meta(get_the_ID(), '_wp_page_template', true);
+function mytheme_load_template( $template ) {
+	if ( is_page() ) {
+		$meta = get_post_meta( get_the_ID(), '_wp_page_template', true );
 
-        if ($meta && file_exists(get_template_directory() . '/' . $meta)) {
-            return get_template_directory() . '/' . $meta;
-        }
-    }
-    return $template;
+		if ( $meta && file_exists( get_template_directory() . '/' . $meta ) ) {
+			return get_template_directory() . '/' . $meta;
+		}
+	}
+
+	return $template;
 }
-add_filter('template_include', 'mytheme_load_template');
+add_filter( 'template_include', 'mytheme_load_template' );
+
+/**
+ * Register custom ACF Gutenberg blocks.
+ *
+ * This function registers all custom blocks defined in the theme.
+ * Each block folder should contain a `block.json` and a `render.php`.
+ *
+ * @return void
+ */
+function mytheme_register_blocks() {
+	$blocks = ['hero', 'slider']; // add future blocks here
+	foreach ($blocks as $block) {
+		register_block_type(get_template_directory() . '/blocks/' . $block);
+	}
+}
+add_action('init', 'mytheme_register_blocks');
